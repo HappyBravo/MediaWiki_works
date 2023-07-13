cf67fefa95ec75c72ff44d0b358a811b16a03005
mHappiee <83241650+HappyBravo@users.noreply.github.com>
2023-07-14 03:20:54 +0530

    Add files via upload
    - added try - except blocks to handle errors
    - bug fixing regarding login of user from terminal
    - added messages to monitor the code's processing
    to do
    - bug with login still persist if it runs with cronjob/crontab/cron.

--- MW_pybot.py
+++ MW_pybot.py
@@ -3,6 +3,7 @@
 import os
 from pathlib import Path
 import pywikibot
+# from pywikibot.data.api import LoginManager
 from OCR_worker.pdftotext import convert_pdf_to_text, extract_text # IMPORTING PYTHON SCRIPTs WHICH ADDS A TEXT LAYER
 
 # DEFINING FUNCTIONS
@@ -33,19 +34,65 @@ def upload_pdf_files(file_path, wiki_url, username, password, addSummary = False
         ELSE, IT SCRAPS THE TEXT FROM PDF   AND ADDS THE TEXT IN THE COMMENT SECTION
     '''
 
-    site = pywikibot.Site(user=username, code="en", fam="mediawiki2") # DEPENDING ON HOW YOU HAVE CONFIGURED YOUR MEDIAWIKI, 'code' AND 'fam' CAN TAKE DIFFERENT VALUES
+    site = pywikibot.Site(user=username,fam="mediawiki2", code="en") # DEPENDING ON HOW YOU HAVE CONFIGURED YOUR MEDIAWIKI, 'code' AND 'fam' CAN TAKE DIFFERENT VALUES
                                                                      # VISIT : https://www.mediawiki.org/wiki/Manual:Pywikibot/Use_on_third-party_wikis
                                                                      # GENERALLY THE CUSTOM WIKI FOLDER IS AT ~/[PYWIKIBOT INSTALL PATH]pywikibot/pywikibot/families ON THE DEVICE
-
+    print("Trying to Login")
     if password == None:
-        site.login() # IN THIS METHOD PASSWORD IS ASKED BY PYWIKIBOT DURING RUNTIME. 
+        message = "Password will be asked by pywikibot as an Input"
+        print(message)
+        try :
+            site.login() # IN THIS METHOD PASSWORD IS ASKED BY PYWIKIBOT DURING RUNTIME. 
+        except Exception as e:
+            print("Login error 1 :", e)
     else:
-        logg = pywikibot.login.LoginManager(password = password, site = site, user = username)
+        try :
+            logg = pywikibot.login.LoginManager(password = password, site = site, user = username)
+            # logg.login()
+        except Exception as e:
+            print("Login error 2 :", e)
+    
         # print(logg)
         # input()
 
+    print("Checking if correctly logged in")
+    login_flagg = 0 # if login_flagg == 3, IT MEANS USER IS LOGGED IN, IS NOT None AND HAS RIGHT TO UPLOAD.
+    print(site.logged_in())
+    # input()
+
+    if site.logged_in():
+        print("User is logged in\nChecking info ...")
+        login_flagg += 1
+
+        u_info = site.userinfo
+        # print(u_info['name'])
+        # print(u_info['rights'])
+
+        mess_str = ""
+        if u_info['name'] != None :
+            mess_str += f"{u_info['name']} is logged in, "
+            login_flagg += 1
+            if 'upload' in u_info['rights']:
+                mess_str += "and has right to upload."
+                login_flagg += 1
+            else:
+                mess_str += "but does not has right to upload."
+        else:
+            mess_str += f"Not logged in as correct user. It is logged in as {u_info['name']}."
+        
+        print(mess_str)
+ 
+    # input()
+    if login_flagg != 3:
+        '''
+        NOT TO INITIATE THE PROCESSING AND UPLOADING TASK IF NOT CORRECTLY LOGGED IN
+        '''
+
+        print("Something is wrong with login !!! \n Try again !!!")
+        return 0 
+    
     for file_name in os.listdir(file_path):
-        if file_name.endswith('.pdf'):
+        if file_name.endswith('.pdf'): # ONLY UPLOAD PDFs FROM THIS SCRIPT
             (new_file_name, isChanged) = check_MIME_pdf(file_name)
             
             print("processing ", {file_name})
@@ -65,7 +112,6 @@ def upload_pdf_files(file_path, wiki_url, username, password, addSummary = False
 
             page_name = file_name[:-4]  # Remove the '.pdf' extension from the file name
 
-            
             if addSummary:
                 if len(summary) < 2:
                     textt = extract_text(output_file_path)
@@ -78,8 +124,10 @@ def upload_pdf_files(file_path, wiki_url, username, password, addSummary = False
             try:
                 page.upload(output_file_path, ignore_warnings=True, comment=summary)
             except Exception as e:
-                print(e)
-    site.logout()
+                print("Error while uploading on MediaWiki, \n --- with Error :", e)
+    
+    # site.logout()
+    return 1
     
 
 # REQUIRED INFO FOR LOGIN AND FILE PATH
@@ -105,7 +153,10 @@ def main_uploader(
     # add_summ = False
     
     # UPLOAD
-    upload_pdf_files(file_path = file_path, wiki_url = wiki_url, username = username,password = password, addSummary = addSummary, summary = summary)
+    # print("Username: ", username)
+    # print("Password: ", password)
+
+    return upload_pdf_files(file_path = file_path, wiki_url = wiki_url, username = username,password = password, addSummary = addSummary, summary = summary)
 
 
 
